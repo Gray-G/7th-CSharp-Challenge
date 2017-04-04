94d17a33dc9ced8a034d902f53a17b658362eea1
Gray-G <graygolden@gmail.com>
2017-04-04 13:46:06 -0700

    Commit refactor of spinReel()
    Refactors spinReel() into multiple helper functions. Commits a working
    display of three random images upon button click event.

--- MegaChallengeCasino2/MegaChallengeCasino2/Default.aspx.cs
+++ MegaChallengeCasino2/MegaChallengeCasino2/Default.aspx.cs
@@ -21,24 +21,49 @@ namespace MegaChallengeCasino2
 
         private void spinReel()
         {
-            // When button is clicked, display a random image in firstImage.
-            string[] imageNameArray = new string[] 
-            {"Strawberry", "Bar", "Lemon",
-                "Bell", "Clover", "Cherry", "Diamond", "Orange",
-                "Seven", "HorseShoe", "Plum", "Watermelon"};
+            // Populate an array with the image names
+            string[] imageNameArray = new string[12];
+            populateImageNameArray(out imageNameArray);
 
             Random randomNumber = new Random();
 
+            // Array to store the three random image names to be displayed
             string[] imageDisplayArray = new string[3];
 
-            for (int i = 0; i < imageDisplayArray.Length; i++)
-            {
-                imageDisplayArray[i] = imageNameArray[randomNumber.Next(11)]; // 0 to 11 images
-            }   
+            // Get three random images
+            getThreeImages(imageNameArray, imageDisplayArray);
 
+            // Assign image Urls to server controls
+            assignImages(imageDisplayArray);
+        }
+
+        private void populateImageNameArray(out string[] imageNameArray)
+        {
+            imageNameArray = new string[] {"Strawberry", "Bar", "Lemon",
+                "Bell", "Clover", "Cherry", "Diamond", "Orange",
+                "Seven", "HorseShoe", "Plum", "Watermelon"};
+        }
+
+        private void assignImages(string[] imageDisplayArray)
+        {
+            // Concatenate a directory path as a string for the three random 
+            // images names and assign it as the imageUrl to server controls
             firstImage.ImageUrl = "/Images/" + imageDisplayArray[0] + ".png";
             secondImage.ImageUrl = "/Images/" + imageDisplayArray[1] + ".png";
             thirdImage.ImageUrl = "/Images/" + imageDisplayArray[2] + ".png";
+
+            return;
+        }
+
+        private void getThreeImages(string[] imageNameArray, string[] imageDisplayArray)
+        {
+            Random randomNumber = new Random();
+
+            // Select three random image names and store them in imageDisplayArray[]
+            for (int i = 0; i < imageDisplayArray.Length; i++)
+            {
+                imageDisplayArray[i] = imageNameArray[randomNumber.Next(11)]; // 0 to 11 images
+            }
         }
     }
 }
